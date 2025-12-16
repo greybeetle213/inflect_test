6563aad9e09970c0c22a7c45061a40dec6a6007c
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2025-12-17 12:21:48 +1300

    Add files via upload

---
+++ dict_US/extras.js
@@ -0,0 +1,13 @@
+function addExtrasToDict(){
+    extras = [
+        ["i", "me", "my", "mine"], 
+        ["you", "your"], 
+        ["he", "him", "his"],
+        ["she", "her"],
+        ["they", "them", "their"],
+        ["it", "its"],
+        ["we", "us", "our"],
+        ["be", "is", "was", "am", "are", "were", "been", "being"],
+        ["a","an"]
+    ]
+}
